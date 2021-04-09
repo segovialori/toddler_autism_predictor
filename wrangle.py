6d3c2d36d0f2cf404eab54031c477bc6b9c38679
Lori Segovia <segovialori@gmail.com>
2021-04-09 10:39:12 -0500

    wrangle first commit

---
+++ wrangle.py
@@ -0,0 +1,10 @@
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
+from sklearn.model_selection import train_test_split
+
+def get_autism_data():
+    
